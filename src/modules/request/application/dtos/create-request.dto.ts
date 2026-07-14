18f4e4aec40a7f6a42567b504cea901f19f4a414
Martin S. Herrera <martinsanher2511@gmail.com>
2026-07-13 21:00:25 -0600

    feat: agregar mensajes de validacion al DTO de request

--- src/modules/request/application/dtos/create-request.dto.ts
+++ src/modules/request/application/dtos/create-request.dto.ts
@@ -1,23 +1,24 @@
 import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateRequestDto {
-  @IsString()
-  @IsNotEmpty()
+  
+  @IsString({ message: 'El nombre debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
   nombre: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Los apellidos deben ser una cadena de texto' })
+  @IsNotEmpty({ message: 'Los apellidos son obligatorios' })
   apellidos: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'El teléfono debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El teléfono es obligatorio' })
   telefono: string;
 
-  @IsEmail()
-  @IsNotEmpty()
+  @IsEmail({}, { message: 'El correo debe tener un formato válido' })
+  @IsNotEmpty({ message: 'El correo es obligatorio' })
   correo: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'El ID de la propiedad debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El ID de la propiedad es obligatorio' })
   propertyId: string;
 }
