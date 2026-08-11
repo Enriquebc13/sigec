bf1e41eabaeb4c73449f732543a2b51190406dc7
Enriquebc13 <baezenrique114@gmail.com>
2026-08-11 16:42:01 -0600

    Configuración de Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,19 @@ async function bootstrap() {
       },
     }),
   );
+
+  // Configuracion de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('SIGEC API')
+    .setDescription('API del Sistema Integral de Gestión de Espacios Comerciales',)
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  //Ruta
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
